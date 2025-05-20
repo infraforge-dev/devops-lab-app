459bd65df6e7df95888ee1cb69ee87d3680ab559
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2025-05-20 16:26:05 -0700

    Create Get_Products_Return_List() and Update_Product_Success()

--- Tests/API.IntegrationTests/API.IntegrationTests/ProductsTests.cs
+++ Tests/API.IntegrationTests/API.IntegrationTests/ProductsTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 using Core.Entities;
 using FluentAssertions;
@@ -43,6 +44,39 @@ namespace API.IntegrationTests
             retrieved.Should().NotBeNull();
             retrieved.Name.Should().Be("Keyboard");
             retrieved.Brand.Should().Be("Logitech");
+        }
+
+        [Fact]
+        public async Task Get_Products_Return_List()
+        {
+            var product1 = await _client.PostAsJsonAsync("/api/v1/products", GetSampleProduct("Monitor"));
+            var product2 = await _client.PostAsJsonAsync("/api/v1/products", GetSampleProduct("Webcam"));
+
+            var response = await _client.GetAsync("/api/v1/products");
+            response.EnsureSuccessStatusCode();
+
+            var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+            products.Should().Contain(p => p.Name == "Monitor");
+            products.Should().Contain(p => p.Name == "Webcam");
+        }
+
+        [Fact]
+        public async Task Update_Product_Success()
+        {
+            var product = await _client.PostAsJsonAsync("/api/v1/products", GetSampleProduct("SSD"));
+            var created = await product.Content.ReadFromJsonAsync<Product>();
+
+            created!.Price = 99.99m;
+            created.QuantityInStock = 10;
+
+            var updateResponse = await _client.PutAsJsonAsync($"/api/v1/products/{created.Id}", created);
+            updateResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+            var getResponse = await _client.GetAsync($"/api/v1/products/{created!.Id}");
+            var updated = await getResponse.Content.ReadFromJsonAsync<Product>();
+
+            updated!.Price.Should().Be(99.99m);
+            updated.QuantityInStock.Should().Be(10);
         }
     }
 }
