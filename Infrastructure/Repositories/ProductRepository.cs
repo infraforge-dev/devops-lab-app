5391fb22d8bfddf04c579ee2df5a12b6f2d7fa1c
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2025-05-21 12:30:06 -0700

    Feature: implement product repository; update products controller; update ProductsTest to reflect change in status code

---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,45 @@
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository(ProductsDbContext context) : IProductRepository
+    {
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+        public void AddProduct(Product product)
+        {
+            context.Products.Add(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            context.Entry(product).State = EntityState.Modified;
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            context.Products.Remove(product);
+        }
+
+        public bool ProductExists(int id)
+        {
+            return context.Products.Any(p => p.Id == id);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+    }
+}
