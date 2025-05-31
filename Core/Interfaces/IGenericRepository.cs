98e2a72df75473f7fce98db8e4cd319e35b21fd4
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2025-05-30 22:36:15 -0700

    feature: generic products repo with specification pattern

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,30 @@
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T>
+        where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(int id);
+
+        Task<IReadOnlyList<T>> ListAllAsync();
+
+        Task<T?> GetEntityWithSpec(ISpecification<T> spec);
+
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+
+        Task<TResult?> GetEntityWithSpec<TResult>(IProjectedSpecification<T, TResult> spec);
+
+        Task<IReadOnlyList<TResult>> ListAsync<TResult>(IProjectedSpecification<T, TResult> spec);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Remove(T entity);
+
+        Task<bool> SaveAllAsync();
+
+        bool Exists(int id);
+    }
+}
