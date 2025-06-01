f1feac95a15057cef68fd114ff7ad1832e0238d9
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2025-05-31 17:06:17 -0700

    fix: updated Pagination<T> to use standard public props. Ensures compatibility with deserialization in integration tests.

--- API/RequestHelpers/Pagination.cs
+++ API/RequestHelpers/Pagination.cs
@@ -1,14 +1,24 @@
 namespace API.RequestHelpers
 {
-    public class Pagination<T>(int pageIndex, int pageSize, int count, IReadOnlyList<T> data)
+    public class Pagination<T>
     {
-        public int PageIndex { get; set; } = pageIndex;
+        public Pagination() { }
 
-        public int PageSize { get; set; } = pageSize;
+        public Pagination(int pageIndex, int pageSize, int totalCount, IReadOnlyList<T> data)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+            Data = data;
+        }
 
-        public int TotalCount { get; set; } = count;
+        public int PageIndex { get; set; }
 
-        public IReadOnlyList<T> Data { get; set; } = data;
+        public int PageSize { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public IReadOnlyList<T>? Data { get; set; }
 
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
     }
