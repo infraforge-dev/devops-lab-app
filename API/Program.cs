eee1a50db2a6032ed5bcd278508f68e5a52de6b5
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2025-06-03 20:28:23 -0700

    feature: add cors

--- API/Program.cs
+++ API/Program.cs
@@ -15,12 +15,19 @@ builder.Services.AddDbContext<ProductsDbContext>(opt =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 // Create database and apply migrations if needed
